a4b21b9f1bdcc9e36c171fe4c7cf8c4e0d2d9239
Ethereumistic <badjarovv@gmail.com>
2023-06-15 18:35:30 +0300

    halfway ready blog backend setup1/2

---
+++ src/app/components/BlogCard.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+// Define the shape of props that BlogCard accepts
+interface BlogCardProps {
+  title: string;
+  author: {
+    name: string;
+    avatar: {
+      url: string;
+    };
+  };
+  coverPhoto: {
+    publishedAt: {
+      createdBy: {
+        id: string;
+      };
+      url: string;
+    };
+  };
+  datePublished: string;
+  slug: string;
+  key: string;
+}
+
+const BlogCard: React.FC<BlogCardProps> = ({ title, author, coverPhoto, datePublished, slug }) => {
+  return (
+    <div className="border border-gray-300 p-4 rounded-md mb-4">
+      <img className="w-full h-64 object-cover rounded-md" src={coverPhoto.publishedAt.url} alt="Cover photo" />
+      <h2 className="text-xl font-bold mt-2">{title}</h2>
+      <div className="flex items-center mt-2">
+        <img className="w-8 h-8 rounded-full mr-2" src={author.avatar.url} alt="Author avatar" />
+        <p>{author.name}</p>
+      </div>
+      <p className="mt-2 text-gray-600">Published on {new Date(datePublished).toLocaleDateString()}</p>
+      <a className="mt-4 inline-block px-4 py-2 bg-blue-600 text-white rounded-md" href={`/blog/${slug}`}>Read more</a>
+    </div>
+  );
+};
+
+export default BlogCard;
--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -2,16 +2,106 @@
 import React from 'react'
 import Navbar from '../navbar/page'
 import TextBox from '../components/TextBox'
-import { collection, addDoc } from "firebase/firestore"
+import BlogCard from '../components/BlogCard'
+import { GraphQLClient, gql } from 'graphql-request'
 
-const News = () => {
+const graphcms = new GraphQLClient('https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clix7xwnx075v01us5z6wehix/master');
+
+const QUERY = gql`
+{
+  posts{
+    id,
+    title,
+    datePublished,
+    slug,
+    content {
+      html
+    }
+    author{
+      name,
+      avatar{
+        url
+      }
+    }
+    coverPhoto{
+      publishedAt{
+        createdBy{
+          id
+        }
+        url
+      }
+    }
+  }
+}`
+
+// define your interfaces here
+interface Post {
+  id: string;
+  title: string;
+  datePublished: string;
+  slug: string;
+  content: {
+    html: string;
+  };
+  author: {
+    name: string;
+    avatar: {
+      url: string;
+    };
+  };
+  coverPhoto: {
+    publishedAt: {
+      createdBy: {
+        id: string;
+      };
+      url: string;
+    };
+  };
+}
+
+interface QueryResult {
+  posts: Post[];
+}
+
+export async function getStaticProps() {
+  const data: QueryResult = await graphcms.request(QUERY);
+  const { posts } = data;
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+
+
+// define the News component
+export default function News({ posts }: any) {
+  // If posts are not available or not an array, display a message
+  if (!posts || !Array.isArray(posts)) {
+    return <div>Loading or no posts available...</div>;
+  }
+
+  // If posts are available, map over them and display a BlogCard for each post
   return (
-    <div>
-        
-            
-      <h1 className='text-3xl font-oswald'></h1>
+    <div className=''>
+      <h1 className='text-3xl font-oswald mt-40'>Blog</h1>
+      <div>
+        {posts.map((post: any, index: number) => (
+          <BlogCard 
+            title={post.title} 
+            author={post.author} 
+            coverPhoto={post.coverPhoto} 
+            key={post.key} // for now, use index as key
+            datePublished={post.datePublished} 
+            slug={post.slug} 
+          />
+        ))}
+      </div>
     </div>
   )
 }
 
-export default News
+
+
